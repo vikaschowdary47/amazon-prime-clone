d97affecef2d6ae0787fc0bc7b56ba61cc3fb4ed
vikaschowdary47 <vikaschowdary47@gmail.com>
2020-08-03 14:05:28 +0530

    search

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
 import logo from '../images/prime.svg'
 import icon from '../images/search.png'
 
 export const Navigation = () => {
+  const [showForm,setShowForm] = React.useState(false);
+
     return (
 
-        <Navbar sticky="top" style={{backgroundColor:"#1a242f",width:"100%"}}>
+        <Navbar sticky="top" style={{backgroundColor:"#1a242f",width:"100%"}} className="justify-content-between">
         <Navbar.Brand href="#home">
           <img
             src={logo}
@@ -16,7 +19,14 @@ export const Navigation = () => {
             alt="React Bootstrap logo"
           />
         </Navbar.Brand>
-        <div className="float-right"><img src={icon} width='30' height='30'/></div>
+        <div style={{marginRight:'20px',display:'flex',flexDirection:'row-reverse'}}>
+          <img src={icon} width='30' height='30'/>
+          <div className="search__form">
+          <Form inline>
+        <Form.Control type="text" placeholder="Search" className=" mr-sm-5" />
+        </Form>
+        </div>
+          </div>
       </Navbar>
     )
 }
